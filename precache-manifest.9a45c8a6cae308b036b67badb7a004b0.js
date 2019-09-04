c1c24f1f0f6f822acb28205e38e46d40c75269d0
Javier Soria <javier.soria@gmail.com>
2019-09-04 13:50:14 -0700

    Updates

---
+++ precache-manifest.9a45c8a6cae308b036b67badb7a004b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52668efda66b37b349a993199aef2fec",
+    "url": "/noteful/index.html"
+  },
+  {
+    "revision": "e385ef36c1c27b10f0bd",
+    "url": "/noteful/static/css/main.5b68862c.chunk.css"
+  },
+  {
+    "revision": "2e836df5608f2d199905",
+    "url": "/noteful/static/js/2.83ce64b4.chunk.js"
+  },
+  {
+    "revision": "e385ef36c1c27b10f0bd",
+    "url": "/noteful/static/js/main.84e66a6f.chunk.js"
+  },
+  {
+    "revision": "7151be588e9b932b8c38",
+    "url": "/noteful/static/js/runtime~main.88443d30.js"
+  }
+]);
